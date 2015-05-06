87e2ad1d2c686718d2dc188fb58daf9de87d9948
Holger Balow-Schott <holger.balow-schott@t-systems-mms.com>
2015-05-06 21:29:57 +0200

    fixes #2

--- src/main/java/com/tsystems/cargo/container/wso2/deployable/CarbonApplication.java
+++ src/main/java/com/tsystems/cargo/container/wso2/deployable/CarbonApplication.java
@@ -1,36 +1,82 @@
 package com.tsystems.cargo.container.wso2.deployable;
 
+import java.io.File;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.codehaus.cargo.container.deployable.DeployableException;
 import org.codehaus.cargo.container.deployable.DeployableType;
+import org.codehaus.cargo.module.JarArchive;
+import org.codehaus.cargo.module.JarArchiveIo;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 /**
  * A WSO2 application deployable. Matches
- * https://docs.wso2.com/display/Carbon420/C-App+Deployment+Process packaging
- * type.
+ * https://docs.wso2.com/display/Carbon420/C-App+Deployment+Process packaging type.
  */
-public class CarbonApplication extends AbstractWSO2Deployable implements
-	WSO2Deployable {
+public class CarbonApplication extends AbstractWSO2Deployable implements WSO2Deployable
+{
 
-    public static final DeployableType TYPE = DeployableType
-	    .toType("carbon/application");
+    public static final DeployableType TYPE = DeployableType.toType("carbon/application");
 
     private String applicationName;
 
-    public CarbonApplication(final String file) {
-	super(file);
+    public CarbonApplication(final String file)
+    {
+        super(file);
     }
 
-    public final String getApplicationName() {
-	if (applicationName == null || applicationName.length() == 0) {
-	    applicationName = parseApplication(".car");
-	}
-	return applicationName;
+    public final String getApplicationName()
+    {
+        if (applicationName == null || applicationName.length() == 0)
+        {
+            parseApplicationName();
+        }
+        return applicationName;
     }
 
-    public DeployableType getType() {
-	return TYPE;
+    public final void parseApplicationName()
+    {
+        try
+        {
+            JarArchive jarArchive = JarArchiveIo.open(new File(getFile()));
+            InputStream in = jarArchive.getResource("artifacts.xml");
+
+            Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
+            XPath xPath = XPathFactory.newInstance().newXPath();
+
+            XPathExpression xPathName =
+                xPath.compile("//artifacts/artifact[1][@type='carbon/application']/@name");
+            Node nodeName = (Node) xPathName.evaluate(doc, XPathConstants.NODE);
+            String nameString = nodeName.getNodeValue();
+
+            XPathExpression xPathVersion =
+                xPath.compile("//artifacts/artifact[1][@type='carbon/application']/@version");
+            Node nodeVersion = (Node) xPathVersion.evaluate(doc, XPathConstants.NODE);
+            String versionString = nodeVersion.getNodeValue();
+
+            applicationName = nameString + "_" + versionString;
+
+        }
+        catch (Exception e)
+        {
+            throw new DeployableException("can not parse carbon application name", e);
+        }
     }
 
-    public void setApplicationName(String applicationName) {
-	this.applicationName = applicationName;
+    public DeployableType getType()
+    {
+        return TYPE;
+    }
+
+    public void setApplicationName(String applicationName)
+    {
+        this.applicationName = applicationName;
     }
 }
--- src/test/java/com/tsystems/cargo/container/wso2/deployable/WSO2DeployableTest.java
+++ src/test/java/com/tsystems/cargo/container/wso2/deployable/WSO2DeployableTest.java
@@ -2,7 +2,6 @@ package com.tsystems.cargo.container.wso2.deployable;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Random;
 import java.util.UUID;
 
 import org.codehaus.cargo.container.deployable.DeployableType;
@@ -13,106 +12,141 @@ import org.junit.Test;
 
 import com.tsystems.cargo.container.wso2.WSO2Carbon4xContainer;
 
-public class WSO2DeployableTest {
+public class WSO2DeployableTest
+{
 
     private static final DeployableFactory DEPLOYABLE_FACTORY = new DefaultDeployableFactory();
 
     @Test
-    public void testWarApplicationName() {
-	WSO2WAR deployable;
+    public void testWarApplicationName()
+    {
+        WSO2WAR deployable;
 
-	deployable = (WSO2WAR) DEPLOYABLE_FACTORY.createDeployable(
-		WSO2Carbon4xContainer.ID, "test.war", DeployableType.WAR);
+        deployable =
+            (WSO2WAR) DEPLOYABLE_FACTORY.createDeployable(WSO2Carbon4xContainer.ID, "test.war",
+                DeployableType.WAR);
 
-	Assert.assertEquals("test.war", deployable.getApplicationName());
+        Assert.assertEquals("test.war", deployable.getApplicationName());
 
-	deployable.setContext("berlin");
-	Assert.assertEquals("berlin.war", deployable.getApplicationName());
+        deployable.setContext("berlin");
+        Assert.assertEquals("berlin.war", deployable.getApplicationName());
 
-	deployable.setVersion("4.1-SNAPSHOT");
-	Assert.assertEquals("berlin#4.1-SNAPSHOT.war",
-		deployable.getApplicationName());
+        deployable.setVersion("4.1-SNAPSHOT");
+        Assert.assertEquals("berlin#4.1-SNAPSHOT.war", deployable.getApplicationName());
 
-	// expanded
-	String name = UUID.randomUUID().toString();
-	File file = new File(new File(System.getProperty("java.io.tmpdir")),
-		name);
-	file.deleteOnExit();
-	file.mkdir();
-	deployable = (WSO2WAR) DEPLOYABLE_FACTORY.createDeployable(
-		WSO2Carbon4xContainer.ID, file.getPath(), DeployableType.WAR);
-	Assert.assertEquals(name, deployable.getApplicationName());
+        // expanded
+        String name = UUID.randomUUID().toString();
+        File file = new File(new File(System.getProperty("java.io.tmpdir")), name);
+        file.deleteOnExit();
+        file.mkdir();
+        deployable =
+            (WSO2WAR) DEPLOYABLE_FACTORY.createDeployable(WSO2Carbon4xContainer.ID,
+                file.getPath(), DeployableType.WAR);
+        Assert.assertEquals(name, deployable.getApplicationName());
 
-	deployable.setContext("berlin");
-	Assert.assertEquals("berlin", deployable.getApplicationName());
+        deployable.setContext("berlin");
+        Assert.assertEquals("berlin", deployable.getApplicationName());
 
-	deployable.setVersion("4.1-SNAPSHOT");
-	Assert.assertEquals("berlin#4.1-SNAPSHOT",
-		deployable.getApplicationName());
+        deployable.setVersion("4.1-SNAPSHOT");
+        Assert.assertEquals("berlin#4.1-SNAPSHOT", deployable.getApplicationName());
     }
 
     @Test
-    public void testCarbonApplicationName() {
-	CarbonApplication deployable;
+    public void testCarbonApplicationName()
+    {
+        CarbonApplication deployable;
 
-	deployable = (CarbonApplication) DEPLOYABLE_FACTORY.createDeployable(
-		WSO2Carbon4xContainer.ID, "test.car", CarbonApplication.TYPE);
+        URL car = ClassLoader.getSystemResource("capp-1.0.0.car");
 
-	Assert.assertEquals("test", deployable.getApplicationName());
+        deployable =
+            (CarbonApplication) DEPLOYABLE_FACTORY.createDeployable(WSO2Carbon4xContainer.ID,
+                car.getFile(), CarbonApplication.TYPE);
+
+        Assert.assertEquals("Capp_1.0.0", deployable.getApplicationName());
+
+        deployable.setApplicationName("berlin");
+        Assert.assertEquals("berlin", deployable.getApplicationName());
     }
 
     @Test
-    public void testAxis2ModuleApplicationName() {
-	Axis2Module deployable;
+    public void testAxis2ModuleApplicationName()
+    {
+        Axis2Module deployable;
 
-	URL mar = ClassLoader.getSystemResource("rampart-1.6.1-wso2v10.mar");
+        URL mar = ClassLoader.getSystemResource("rampart-1.6.1-wso2v10.mar");
 
-	deployable = (Axis2Module) DEPLOYABLE_FACTORY.createDeployable(
-		WSO2Carbon4xContainer.ID, mar.getFile(), Axis2Module.TYPE);
+        deployable =
+            (Axis2Module) DEPLOYABLE_FACTORY.createDeployable(WSO2Carbon4xContainer.ID,
+                mar.getFile(), Axis2Module.TYPE);
 
-	Assert.assertEquals("rampart", deployable.getApplicationName());
+        Assert.assertEquals("rampart", deployable.getApplicationName());
+
+        deployable.setApplicationName("berlin");
+        Assert.assertEquals("berlin", deployable.getApplicationName());
     }
 
     @Test
-    public void testAxis2ServiceApplicationName() {
-	Axis2Service deployable;
+    public void testAxis2ServiceApplicationName()
+    {
+        Axis2Service deployable;
 
-	deployable = (Axis2Service) DEPLOYABLE_FACTORY.createDeployable(
-		WSO2Carbon4xContainer.ID, "test.aar", Axis2Service.TYPE);
+        deployable =
+            (Axis2Service) DEPLOYABLE_FACTORY.createDeployable(WSO2Carbon4xContainer.ID,
+                "berlin.aar", Axis2Service.TYPE);
 
-	Assert.assertEquals("test", deployable.getApplicationName());
+        Assert.assertEquals("berlin", deployable.getApplicationName());
+
+        deployable.setApplicationName("london");
+        Assert.assertEquals("london", deployable.getApplicationName());
     }
 
     @Test
-    public void testWSO2Axis2ServiceApplicationName() {
-	WSO2Axis2Service deployable;
+    public void testWSO2Axis2ServiceApplicationName()
+    {
+        WSO2Axis2Service deployable;
 
-	deployable = (WSO2Axis2Service) DEPLOYABLE_FACTORY.createDeployable(
-		WSO2Carbon4xContainer.ID, "test.aar", WSO2Axis2Service.TYPE);
+        deployable =
+            (WSO2Axis2Service) DEPLOYABLE_FACTORY.createDeployable(WSO2Carbon4xContainer.ID,
+                "berlin.aar", WSO2Axis2Service.TYPE);
 
-	Assert.assertEquals("test", deployable.getApplicationName());
+        Assert.assertEquals("berlin", deployable.getApplicationName());
+
+        deployable.setApplicationName("tokio");
+        Assert.assertEquals("tokio", deployable.getApplicationName());
     }
 
     @Test
-    public void testBAMToolboxApplicationName() {
-	BAMToolbox deployable;
+    public void testBAMToolboxApplicationName()
+    {
+        BAMToolbox deployable;
 
-	deployable = (BAMToolbox) DEPLOYABLE_FACTORY.createDeployable(
-		WSO2Carbon4xContainer.ID, "test.tbox", BAMToolbox.TYPE);
+        deployable =
+            (BAMToolbox) DEPLOYABLE_FACTORY.createDeployable(WSO2Carbon4xContainer.ID,
+                "berlin.tbox", BAMToolbox.TYPE);
 
-	Assert.assertEquals("test", deployable.getApplicationName());
+        Assert.assertEquals("berlin", deployable.getApplicationName());
+
+        deployable.setApplicationName("paris");
+        Assert.assertEquals("paris", deployable.getApplicationName());
     }
 
     @Test
-    public void testWSO2ConnectorApplicationName() {
-	WSO2Connector deployable;
+    public void testWSO2ConnectorApplicationName()
+    {
+        WSO2Connector deployable;
 
-	URL zip = ClassLoader.getSystemResource("twitter-connector-1.0.0.zip");
+        URL zip = ClassLoader.getSystemResource("twitter-connector-1.0.0.zip");
 
-	deployable = (WSO2Connector) DEPLOYABLE_FACTORY.createDeployable(
-		WSO2Carbon4xContainer.ID, zip.getFile(), WSO2Connector.TYPE);
+        deployable =
+            (WSO2Connector) DEPLOYABLE_FACTORY.createDeployable(WSO2Carbon4xContainer.ID,
+                zip.getFile(), WSO2Connector.TYPE);
 
-	Assert.assertEquals("{org.wso2.carbon.connectors}twitter", deployable.getApplicationName());
+        Assert.assertEquals("{org.wso2.carbon.connectors}twitter",
+            deployable.getApplicationName());
+
+        deployable.setApplicationName("newyork");
+        Assert.assertEquals("{org.wso2.carbon.connectors}twitter",
+            deployable.getApplicationName());
     }
 
 }
